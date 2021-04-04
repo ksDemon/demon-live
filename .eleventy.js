d612f1f18dd7325ff489edcae77d8f62c268c92a
ksDemon <64107178+ksDemon@users.noreply.github.com>
2021-04-04 06:37:02 -0400

    master

--- .eleventy.js
+++ .eleventy.js
@@ -3,16 +3,10 @@ module.exports = function(config) {
   config.addPassthroughCopy("src/js");
   config.addPassthroughCopy("src/css");
   config.addPassthroughCopy("src/msc");
+  config.addPassthroughCopy("src");
   return  {
     dir: {
       input: "src",
-      output: "dist"
-    }
-  };
-  config.addPassthroughCopy("src/media");
-  return  {
-    dir: {
-      input: "src/media",
       output: "dist"
     }
   };
