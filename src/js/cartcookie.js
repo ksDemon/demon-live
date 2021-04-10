e0ff2070760e92dd6af9710f4b3b816904b8a9cc
ksDemon <64107178+ksDemon@users.noreply.github.com>
2021-04-10 13:14:16 -0400

    sus

--- src/js/cartcookie.js
+++ src/js/cartcookie.js
@@ -1,6 +1,17 @@
 ID = document.getElementById("id").innerHTML;
 ID = String(Number(ID) + 1);
 
+var stock;
+
+let request = new XMLHttpRequest();
+request.open("GET", "https://sheets.googleapis.com/v4/spreadsheets/12wZxH_YKZ71AtlY8lrOkjK2DLOgjs9uy0-UfxEVWXKw/values/D" + ID + ":D" + ID + "?key=AIzaSyCzaVfKdBQ6GhatoH_q2JAoaZGn0d9VAl0");
+request.send();
+request.onload = () => {
+    stock = JSON.parse(request.response).values[0][0];
+    document.getElementById("stock").innerHTML = "Disponibilidad: " + JSON.parse(request.response).values[0][0] + " unidades";
+    document.getElementById("cart").innerHTML = String(getCookie(ID));
+}
+
 function getCookie(cname) {
     var name = cname + "=";
     var decodedCookie = decodeURIComponent(document.cookie);
@@ -18,20 +29,38 @@ function getCookie(cname) {
   }
 
 function add() {
+  let request = new XMLHttpRequest();
+  request.open("GET", "https://sheets.googleapis.com/v4/spreadsheets/12wZxH_YKZ71AtlY8lrOkjK2DLOgjs9uy0-UfxEVWXKw/values/D" + ID + ":D" + ID + "?key=AIzaSyCzaVfKdBQ6GhatoH_q2JAoaZGn0d9VAl0");
+  request.send();
+  request.onload = () => {
+    stock = JSON.parse(request.response).values[0][0];
+    document.getElementById("stock").innerHTML = "Disponibilidad: " + JSON.parse(request.response).values[0][0] + " unidades";
     var cantidad = getCookie(ID);
-    if (cantidad == 0) {
+    if (cantidad == 0 && stock != 0) {
         document.cookie = ID + "=1; path=/;"
     }
     else {
-        var aumento = String(Number(cantidad)+1);
+      var aumento = Number(cantidad)+1;
+      if (aumento <= stock) {
         document.cookie = ID + "="  + aumento + "; path=/;";
+      }
     }
+    document.getElementById("cart").innerHTML = String(getCookie(ID));
+  }
 }
 
 function sub() {
-    var cantidad = getCookie(ID);
-    if (cantidad =! 0) {
-        var aumento = String(Number(cantidad)-1);
-        document.cookie = ID + "="  + aumento + "; path=/;";
-    }
+  let request = new XMLHttpRequest();
+  request.open("GET", "https://sheets.googleapis.com/v4/spreadsheets/12wZxH_YKZ71AtlY8lrOkjK2DLOgjs9uy0-UfxEVWXKw/values/D" + ID + ":D" + ID + "?key=AIzaSyCzaVfKdBQ6GhatoH_q2JAoaZGn0d9VAl0");
+  request.send();
+  request.onload = () => {
+      stock = JSON.parse(request.response).values[0][0];
+      document.getElementById("stock").innerHTML = "Disponibilidad: " + JSON.parse(request.response).values[0][0] + " unidades";
+      var cantidad = Number(getCookie(ID));
+      if (cantidad > 0) {
+          var aumento = Number(cantidad)-1;
+          document.cookie = ID + "="  + aumento + "; path=/;";
+      }
+      document.getElementById("cart").innerHTML = String(getCookie(ID));
+  }
 }
