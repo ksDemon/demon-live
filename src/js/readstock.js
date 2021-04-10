d08dffb6058605e5307ef0f1460ed3a259e88add
ksDemon <64107178+ksDemon@users.noreply.github.com>
2021-04-10 01:38:30 -0400

    online stock

--- src/js/readstock.js
+++ src/js/readstock.js
@@ -1,1 +1,9 @@
-GET https://sheets.googleapis.com/v4/spreadsheets/12wZxH_YKZ71AtlY8lrOkjK2DLOgjs9uy0-UfxEVWXKw/values/D2:D2?key=AIzaSyCzaVfKdBQ6GhatoH_q2JAoaZGn0d9VAl0
+ID = document.getElementById("id").innerHTML;
+ID = Number(ID) + 1;
+
+let request = new XMLHttpRequest();
+request.open("GET", "https://sheets.googleapis.com/v4/spreadsheets/12wZxH_YKZ71AtlY8lrOkjK2DLOgjs9uy0-UfxEVWXKw/values/D" + ID + ":D" + ID + "?key=AIzaSyCzaVfKdBQ6GhatoH_q2JAoaZGn0d9VAl0");
+request.send();
+request.onload = () => {
+    document.getElementById("stock").innerHTML = "Disponibilidad: " + JSON.parse(request.response).values[0][0] + " unidades";
+}
