666164336a9ce4b5e3e80a083af62a7a6dbb9020
ksDemon <64107178+ksDemon@users.noreply.github.com>
2021-05-21 11:07:22 -0400

    responsive iframe B)

---
+++ src/js/scale.js
@@ -0,0 +1,22 @@
+div = document.getElementById("gamediv")
+iframe = document.getElementById("pongFrame")
+
+window.addEventListener('resize', () => scaleElements());
+
+function scaleElements() {
+    scaleH = 0.25 * window.innerHeight / 939
+    scaleW = 0.25 * window.innerWidth / 1920
+    if (scaleW < scaleH) {
+        scale = scaleW
+    } else {
+        scale = scaleH
+    }
+    per = scale ** (-1) * 100 + "%"
+    pix = scale * 4 * 750 + "px"
+    div.style.height = pix
+    div.style.width = pix
+    iframe.style.height = per
+    iframe.style.width = per
+    iframe.style.zoom = scale
+    iframe.style.webkitTransform = "scale(" + scale + ")"
+}
